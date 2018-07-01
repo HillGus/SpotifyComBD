8348057f6e1adb5e0cdf3b298de4d4ed00415f1b
HillGus <39138506+HillGus@users.noreply.github.com>
2018-07-01 16:52:12 -0300

    Update spotify.sql

--- spotify.sql
+++ spotify.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.8.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 30-Jun-2018 às 02:51
--- Versão do servidor: 10.1.28-MariaDB
--- PHP Version: 7.1.11
+-- Generation Time: Jul 01, 2018 at 09:45 PM
+-- Server version: 10.1.33-MariaDB
+-- PHP Version: 7.2.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,6 +21,172 @@ SET time_zone = "+00:00";
 --
 -- Database: `spotify`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `album`
+--
+
+create database spotify; 
+use spotify; 
+
+CREATE TABLE `album` (
+  `idAlbum` int(11) NOT NULL,
+  `nomeAlbum` varchar(30) NOT NULL,
+  `idUsuario` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mngrplm`
+--
+
+CREATE TABLE `mngrplm` (
+  `idPlaylist` int(11) NOT NULL,
+  `idMusica` int(11) NOT NULL,
+  `idUsuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `musica`
+--
+
+CREATE TABLE `musica` (
+  `idMusica` int(11) NOT NULL,
+  `nomeMusica` varchar(30) NOT NULL,
+  `idArtista` int(11) NOT NULL,
+  `generoMusica` varchar(30) DEFAULT NULL,
+  `duracaoMusica` time DEFAULT NULL,
+  `idAlbum` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `playlist`
+--
+
+CREATE TABLE `playlist` (
+  `idPlaylist` int(11) NOT NULL,
+  `nomePlaylist` varchar(30) NOT NULL,
+  `idUsuario` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `idUsuario` int(11) NOT NULL,
+  `loginUsuario` varchar(30) NOT NULL,
+  `senhaUsuario` varchar(30) NOT NULL,
+  `artista` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `album`
+--
+ALTER TABLE `album`
+  ADD PRIMARY KEY (`idAlbum`),
+  ADD KEY `idUsuario` (`idUsuario`);
+
+--
+-- Indexes for table `mngrplm`
+--
+ALTER TABLE `mngrplm`
+  ADD KEY `MngrPLM_fk0` (`idPlaylist`),
+  ADD KEY `MngrPLM_fk1` (`idMusica`),
+  ADD KEY `idUsuario` (`idUsuario`);
+
+--
+-- Indexes for table `musica`
+--
+ALTER TABLE `musica`
+  ADD PRIMARY KEY (`idMusica`),
+  ADD KEY `Musica_fk0` (`idArtista`),
+  ADD KEY `idAlbum` (`idAlbum`);
+
+--
+-- Indexes for table `playlist`
+--
+ALTER TABLE `playlist`
+  ADD PRIMARY KEY (`idPlaylist`),
+  ADD KEY `idUsuario` (`idUsuario`);
+
+--
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idUsuario`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `album`
+--
+ALTER TABLE `album`
+  MODIFY `idAlbum` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `musica`
+--
+ALTER TABLE `musica`
+  MODIFY `idMusica` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `playlist`
+--
+ALTER TABLE `playlist`
+  MODIFY `idPlaylist` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `album`
+--
+ALTER TABLE `album`
+  ADD CONSTRAINT `album_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`);
+
+--
+-- Constraints for table `mngrplm`
+--
+ALTER TABLE `mngrplm`
+  ADD CONSTRAINT `MngrPLM_fk0` FOREIGN KEY (`idPlaylist`) REFERENCES `playlist` (`idPlaylist`),
+  ADD CONSTRAINT `MngrPLM_fk1` FOREIGN KEY (`idMusica`) REFERENCES `musica` (`idMusica`),
+  ADD CONSTRAINT `mngrplm_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`);
+
+--
+-- Constraints for table `musica`
+--
+ALTER TABLE `musica`
+  ADD CONSTRAINT `Musica_fk0` FOREIGN KEY (`idArtista`) REFERENCES `usuario` (`idUsuario`),
+  ADD CONSTRAINT `musica_ibfk_1` FOREIGN KEY (`idAlbum`) REFERENCES `album` (`idAlbum`);
+
+--
+-- Constraints for table `playlist`
+--
+ALTER TABLE `playlist`
+  ADD CONSTRAINT `playlist_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
